337d4b5f051feb5873f087c838b208aa6ebe10ee
LorLynnee <germino1820308@mkt.ceu.edu.ph>
2019-01-22 15:41:28 +0800

    new JFrames

--- Draw.java
+++ Draw.java
@@ -16,6 +16,8 @@ public class Draw extends JComponent{
 	public int x = 30;
 	public int y = 30;
 
+	public int state = 0;
+
 	public Draw(){
 		try{
 			image = ImageIO.read(resource);
@@ -24,7 +26,36 @@ public class Draw extends JComponent{
 			e.printStackTrace();
 		}
 	}
+	public void reloadImage(){
+		state++;
 
+		if(state == 0){
+			resource = getClass().getResource("run0.png");
+		}
+		else if(state == 1){
+			resource = getClass().getResource("run01.png");
+		}
+		else if(state == 2){
+			resource = getClass().getResource("run02.png");
+		}
+		else if(state == 3){
+			resource = getClass().getResource("run03.png");
+		}
+		else if(state == 4){
+			resource = getClass().getResource("run04.png");
+		}
+		else if(state == 5){
+			resource = getClass().getResource("run05.png");
+			state = 0;
+		}
+		try{
+			image = ImageIO.read(resource);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+
+	}
 
 	public void moveUp(){
 		y = y - 5;
@@ -51,9 +82,9 @@ public class Draw extends JComponent{
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		g.setColor(Color.YELLOW);
-		g.fillOval(x, y, 50, 50);
 		g.drawImage(image, x, y, this);
 
 	}
 }
+
+	
